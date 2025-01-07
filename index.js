556615d3b6c98ba756a184d16894c3bedc612ecf
Emansafavi <emansafavibayat@gmail.com>
2025-01-07 20:29:55 +0100

    edits on Personal Sound Space Project

--- index.js
+++ index.js
@@ -5,3 +5,21 @@ const navList = document.querySelector('.nav-list');
 hamburger.addEventListener('click', () => {
     navList.classList.toggle('active');
 });
+function scrollleft() {
+    document.querySelector('.image-container').scrollBy({
+        left: -200,
+        behavior: 'smooth'
+      });
+}
+function scrollRight() {
+    document.querySelector('.image-container').scrollBy({
+      left: 200,
+      behavior: 'smooth'
+    });
+  }
+  setInterval(() => {
+    document.querySelector('.image-container').scrollBy({
+      left: 200,
+      behavior: 'smooth'
+    });
+  }, 600); // Adjust the interval as needed
